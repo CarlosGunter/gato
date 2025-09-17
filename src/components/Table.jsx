cd4484ea62402c688e8ba75e864eb3e2c0248117
CarlosGunter <carlosgt1702@gmail.com>
2025-09-17 08:22:48 -0600

    refactor: clean up formatting in Table.jsx and Table.css for improved readability

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,17 +5,20 @@ import { movePC } from '../lib/movePC'
 import './Table.css'
 
 export default function Table ({ currentTurn, setTurn, assign, match, setMatch, win, setWinner }) {
-  // Bloque de codigo que define cuando la computadora le toca hacer
-  // su movimiento
+  // Bloque de codigo que define cuando la computadora le toca hacer su movimiento
   useEffect(() => {
     // En caso de ser turno del usuario o ya existe un ganador no jecuta nada
     if (currentTurn === TURNS.User || win) return
+
     setTimeout(() => {
       // Se invoca la funcion donde se define el movimiento de la computadora
       const gameMachine = movePC({ match, assign })
       setMatch(gameMachine) // Se actualiza el estado del juego
+
       const isWin = checkWin(gameMachine) // Se evalua si hay un ganador
+
       if (isWin === 1) setWinner(1) // Se evalua y define si hay empate
+
       // En caso de que un jugador gane se actualiza el estado ganador
       // caso contrario se le transfiere el turno al usuario
       isWin ? setWinner(isWin) : setTurn(TURNS.User)
@@ -26,14 +29,16 @@ export default function Table ({ currentTurn, setTurn, assign, match, setMatch, 
 
   // Se ejecuta cuando el usuarioo hace click en una casilla
   const moveUser = (isValid, index) => {
-    // Si la casiila esta ocupada, es turno de la computadora, o hay un
-    // ganador no se ejecuta nada
+    // Si la casiila esta ocupada, es turno de la computadora, o hay un ganador no se ejecuta nada
     if (isValid || currentTurn === TURNS.Machine || win) return
+
     const copyGame = [...match]
     copyGame[index] = assign.current.user
     setMatch(copyGame) // Se actualiza estado del juego
+
     const isWin = checkWin(copyGame) // Se evalua si hay un ganador
     if (isWin === 1) setWinner(1) // Se evalua y define si hay empate
+
     // En caso de que un jugador gane se actualiza el estado ganador
     // caso contrario se le transfiere el turno al la computadora
     isWin ? setWinner(isWin) : setTurn(TURNS.Machine)
@@ -48,19 +53,23 @@ export default function Table ({ currentTurn, setTurn, assign, match, setMatch, 
         if (game[4] === game[4 - i] && game[4] === game[4 + i]) return game[4]
       }
     }
+
     // Evalua todas las posible combinaciones ganadoras que contengan los extremos izquierdo y superior del tablero
     if (game[0] && (
       (game[0] === game[1] && game[1] === game[2]) ||
       (game[0] === game[3] && game[3] === game[6])
     )) return game[0]
+
     // Evalua todas las posible combinaciones ganadoras que contenga los lados derecho e inferior del tablero
     if (game[8] && (
       (game[8] === game[2] && game[8] === game[5]) ||
       (game[8] === game[6] && game[8] === game[7])
     )) return game[8]
+
     // En caso de que todo el tablero se encuentre ocupado sin un ganador
     // retorna 1 definiendo como empate el juego
     if (!game.includes(null)) return 1
+
     return false
   }
 
